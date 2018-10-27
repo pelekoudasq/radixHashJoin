128eb13a26e79223d768607628833b62fa84656d
Ioannis Pelekoudas <peleioannis@gmail.com>
2018-10-28 02:02:50 +0300

    complete, without result list

--- join.c
+++ join.c
@@ -14,11 +14,13 @@
 int32_t twoInLSB;
 
 void hashRelation(relInfo* newRel, relation *rel){
+	
 	newRel->histogram = calloc(twoInLSB, sizeof(int32_t));
 	//first run through table
 	for(int32_t i = 0; i < rel->num_tuples; i++){
 		//find hash position and increase histogram value by one
 		int32_t position = rel->tuples[i].payload & (twoInLSB-1);
+
 		newRel->histogram[position]++;
 	}
 
@@ -31,11 +33,11 @@ void hashRelation(relInfo* newRel, relation *rel){
 	//second run through table, new relation table
 	newRel->tups.num_tuples = rel->num_tuples;
 	newRel->tups.tuples = malloc((rel->num_tuples)*sizeof(tuple));
-
 	for(int32_t i = 0; i < rel->num_tuples; i++){
 		//find hash position
 		int32_t position = (rel->tuples[i].payload)&(twoInLSB-1);
 		//copy that tuple to the new table in the right position
+
 		memcpy(newRel->tups.tuples+sumHistogram[position], &rel->tuples[i], sizeof(tuple));
 		//increase position for current hash result value
 		sumHistogram[position]++;
@@ -44,8 +46,9 @@ void hashRelation(relInfo* newRel, relation *rel){
 }
 
 void getBucket(relInfo* small, relInfo* big, int begSmall, int begBig, int bucketNo){
+	
 	int hashValue = nextPrime(small->histogram[bucketNo]);
-	printf("%d, %d\n", hashValue, small->histogram[bucketNo]);
+	
 	//hash small
 	int32_t* bucket = malloc(hashValue * sizeof(int32_t));
 	for(int i = 0; i < hashValue; i++)
@@ -55,16 +58,28 @@ void getBucket(relInfo* small, relInfo* big, int begSmall, int begBig, int bucke
 	int endSmall = begSmall + small->histogram[bucketNo];
 
 	for(int32_t i = begSmall; i < endSmall; i++){
-		//find hash position and increase histogram value by one
+
 		int32_t position = (small->tups.tuples[i].payload) % hashValue;
 		int previousValue = bucket[position];
 		bucket[position] = i-begSmall;
 		chain[i-begSmall] = previousValue;
 	}
+	int endBig = begBig + big->histogram[bucketNo];
+	
+	//run through big bucket, join with small
+	for(int32_t i = begBig; i < endBig; i++){
+
+		int bigHash = (big->tups.tuples[i].payload) % hashValue;
+		int32_t position = bucket[bigHash];
+		while(position != -1){
+			if (big->tups.tuples[i].payload == small->tups.tuples[position+begSmall].payload){
+				printf("YES %d, %d\n", big->tups.tuples[i].payload, small->tups.tuples[position+begSmall].payload);
+			}
+			position = chain[position];
+		}
+	}
 	free(bucket);
 	free(chain);
-	//join
-	
 }
 
 result* RadixHashJoin(relation *relR, relation *relS){
@@ -78,18 +93,19 @@ result* RadixHashJoin(relation *relR, relation *relS){
 
 	int begR = 0, begS = 0;
 	for (int i = 0; i < twoInLSB; i++){
-		if (relRhashed.histogram[i] == 0 || relShashed.histogram[i] == 0)
+		if (relRhashed.histogram[i] == 0 || relShashed.histogram[i] == 0){
+			begR += relRhashed.histogram[i];
+			begS += relShashed.histogram[i];
 			continue;
-		if (relRhashed.histogram[i] > relShashed.histogram[i])
-			getBucket(&relRhashed, &relShashed, begR, begS, i);
-		else 
+		}
+		if (relRhashed.histogram[i] >= relShashed.histogram[i])
 			getBucket(&relShashed, &relRhashed, begS, begR, i);
+		else 
+			getBucket(&relRhashed, &relShashed, begR, begS, i);
 
 		begR += relRhashed.histogram[i];
 		begS += relShashed.histogram[i];
 	}
-
-	printf("%d, %d\n", relRhashed.tups.num_tuples, relShashed.tups.num_tuples);
 
 	free(relRhashed.tups.tuples);
 	free(relShashed.tups.tuples);
