dc84d1c7b72ce05c5554704a8b3451ed3c5fea9c
Ioannis Pelekoudas <peleioannis@gmail.com>
2018-11-12 08:37:09 +0200

    pt1 to auxFun

--- auxFun.c
+++ auxFun.c
@@ -31,3 +31,28 @@ int32_t next_prime(int32_t x) {
 int32_t pow2(int32_t exp){
     return 1 << exp;
 }
+
+relation *tableRelation(int32_t *table, int32_t columnNumber, uint32_t numOfRows, int32_t numOfColumns) {
+    relation *R = malloc(sizeof(relation));
+    R->num_tuples = numOfRows;
+    R->tuples = malloc(numOfRows*sizeof(tuple));
+    for (int32_t i = 0; i < numOfRows; i++){
+        R->tuples[i].key = i + 1;
+        R->tuples[i].payload = table[i*numOfColumns+columnNumber-1];
+    }
+    return R;
+}
+
+relation *randomRel(int x) {
+    relation *R = malloc(sizeof(relation));
+    R->num_tuples = x;
+    R->tuples = malloc(x*sizeof(tuple));
+    if(R->tuples == NULL){
+        fprintf(stderr, ">>>>>>>>>>>>>>>>>>> Fail\n");
+    }
+    for (int32_t i = 0; i < x; i++){
+        R->tuples[i].key = i + 1;
+        R->tuples[i].payload = rand() % 25;
+    }
+    return R;
+}
